da544a0cd47bfe515dc8a18a82f5626c60c806eb
vineet22vishwakarma@gmail.com <vineet22vishwakarma@gmail.com>
2023-10-24 02:06:45 +0530

    Add animation and create default constructors

--- lib/widgets/main_weather_card.dart
+++ lib/widgets/main_weather_card.dart
@@ -2,104 +2,72 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 
 class MainCard extends StatelessWidget {
-  const MainCard({super.key});
+  final String temp;
+  final String feelsLike;
+  final String description;
+  final String imgLocation;
+  const MainCard(
+      {super.key,
+      required this.temp,
+      required this.feelsLike,
+      required this.description,
+      required this.imgLocation});
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 18.0),
-          child: SizedBox(
-            height: 220,
-            width: double.infinity,
-            child: Card(
-              elevation: 10,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.horizontal(
-                  right: Radius.circular(50),
-                  left: Radius.circular(50),
-                ),
-              ),
-              child: Container(
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [
-                      Color.fromARGB(255, 174, 205, 255),
-                      Color.fromARGB(255, 88, 151, 253),
-                    ],
-                  ),
+    return Animate(
+      effects: const [
+        FadeEffect(
+          duration: Duration(milliseconds: 900),
+        ),
+      ],
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 18.0),
+            child: SizedBox(
+              height: 220,
+              width: double.infinity,
+              child: Card(
+                elevation: 10,
+                shape: const RoundedRectangleBorder(
                   borderRadius: BorderRadius.horizontal(
                     right: Radius.circular(50),
                     left: Radius.circular(50),
                   ),
                 ),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        const Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              height: 110,
-                            ),
-                            Text(
-                              'Heavy Rain',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                                fontSize: 22,
-                                shadows: [
-                                  Shadow(
-                                    color: Color.fromARGB(50, 3, 168, 244),
-                                    offset: Offset(0, 5),
-                                    blurRadius: 10,
-                                  ),
-                                ],
+                child: Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        Color.fromARGB(255, 174, 205, 255),
+                        Color.fromARGB(255, 88, 151, 253),
+                      ],
+                    ),
+                    borderRadius: BorderRadius.horizontal(
+                      right: Radius.circular(50),
+                      left: Radius.circular(50),
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const SizedBox(
+                                height: 120,
                               ),
-                            ),
-                            Text(
-                              'Tonight',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                                fontSize: 14,
-                                shadows: [
-                                  Shadow(
-                                    color: Color.fromARGB(50, 3, 168, 244),
-                                    offset: Offset(0, 5),
-                                    blurRadius: 10,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            const SizedBox(
-                              height: 20,
-                            ),
-                            ShaderMask(
-                              shaderCallback: (Rect bounds) {
-                                return const LinearGradient(
-                                    begin: Alignment.topCenter,
-                                    end: Alignment.bottomCenter,
-                                    colors: [
-                                      Colors.white,
-                                      Color.fromARGB(255, 159, 196, 255),
-                                    ]).createShader(bounds);
-                              },
-                              child: const Text(
-                                '27\u00B0',
-                                style: TextStyle(
-                                  fontSize: 72,
+                              Text(
+                                description,
+                                style: const TextStyle(
                                   fontWeight: FontWeight.bold,
                                   color: Colors.white,
+                                  fontSize: 18,
                                   shadows: [
                                     Shadow(
                                       color: Color.fromARGB(50, 3, 168, 244),
@@ -109,41 +77,102 @@ class MainCard extends StatelessWidget {
                                   ],
                                 ),
                               ),
-                            ),
-                            const Icon(
-                              Icons.waves,
-                              color: Color.fromARGB(53, 68, 137, 255),
-                              size: 70,
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
+                              const Text(
+                                'Tonight',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                  shadows: [
+                                    Shadow(
+                                      color: Color.fromARGB(50, 3, 168, 244),
+                                      offset: Offset(0, 5),
+                                      blurRadius: 10,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              ShaderMask(
+                                shaderCallback: (Rect bounds) {
+                                  return const LinearGradient(
+                                      begin: Alignment.topCenter,
+                                      end: Alignment.bottomCenter,
+                                      colors: [
+                                        Colors.white,
+                                        Color.fromARGB(255, 159, 196, 255),
+                                      ]).createShader(bounds);
+                                },
+                                child: Text(
+                                  '$temp\u00B0',
+                                  style: const TextStyle(
+                                    fontSize: 45,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                    shadows: [
+                                      Shadow(
+                                        color: Color.fromARGB(50, 3, 168, 244),
+                                        offset: Offset(0, 5),
+                                        blurRadius: 10,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                'Feels like $feelsLike\u00B0',
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16,
+                                  shadows: [
+                                    Shadow(
+                                      color: Color.fromARGB(50, 3, 168, 244),
+                                      offset: Offset(0, 5),
+                                      blurRadius: 10,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const Icon(
+                                Icons.waves,
+                                color: Color.fromARGB(53, 68, 137, 255),
+                                size: 70,
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        Animate(
-          onPlay: (controller) => controller.repeat(reverse: true),
-          effects: const [
-            MoveEffect(
-              begin: Offset(-5, 0),
-              end: Offset(5, 0),
-              duration: Duration(seconds: 5),
-            ),
-          ],
-          child: Positioned(
-            bottom: 95,
-            left: 50,
-            child: Image.asset(
-              'assets/images/11.png',
-              width: 120,
+          Animate(
+            onPlay: (controller) => controller.repeat(reverse: true),
+            effects: const [
+              MoveEffect(
+                begin: Offset(-20, 0),
+                end: Offset(5, 0),
+                duration: Duration(seconds: 5),
+              ),
+            ],
+            child: Positioned(
+              bottom: 95,
+              left: 50,
+              child: Image.asset(
+                imgLocation,
+                width: 120,
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
